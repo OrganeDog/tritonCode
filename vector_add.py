44fcd2de1cede57142662ecb38694f32b8d597bf
Shujie HU <shujiehu@YiyaoMacBook-Pro.local>
2026-05-25 15:49:55 +0800

    add vector_add triton kernel

---
+++ vector_add.py
@@ -0,0 +1,38 @@
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def add_kernel(x_ptr,
+               y_ptr,
+               output_ptr,
+               n_elements,
+               BLOCK_SIZE: tl.constexpr,
+               ):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+
+def add(x: torch.Tensor, y: torch.Tensor):
+    output = torch.empty_like(x)
+    n_elements = x.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=4096)
+    return output
+
+
+if __name__ == "__main__":
+    size = 100000
+    x = torch.randn(size, device="cuda")
+    y = torch.randn(size, device="cuda")
+    output = add(x, y)
+    torch.testing.assert_close(output, x + y)
+    print("vector_add passed!")
+
